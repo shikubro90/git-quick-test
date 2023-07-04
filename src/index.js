c78eecbabc485708d850bf521e94f2739a16060b
Shiku <95028715+shikubro90@users.noreply.github.com>
2023-07-04 17:30:24 +0600

    version 4 has changes with console multiple time

--- src/index.js
+++ src/index.js
@@ -2,3 +2,9 @@ console.log("index.js");
 console.log('version 4')
 console.log("version 4 has change with 44")
 console.log("though I want to do some other changes but I need to know more");
+console.log("though I want to do some other changes but I need to know more");
+console.log("though I want to do some other changes but I need to know more");
+console.log("though I want to do some other changes but I need to know more");
+console.log("though I want to do some other changes but I need to know more");
+
+
